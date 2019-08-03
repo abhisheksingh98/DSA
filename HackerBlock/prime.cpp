053d847ae2ad5b0b1ed4e6b7e315bc0f4597faf0
Abhishek Singh <33527786+abhisheksingh98@users.noreply.github.com>
2019-08-03 23:54:32 +0530

    Add files via upload

---
+++ HackerBlock/prime.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=2;(i*i)<=n;i++){
+        if(n%i==0){
+            cout<<"Not Prime";
+            return 0;
+        }
+    }
+    cout<<"Prime";
+
+
+    return 0;
+    }
