dc1217ee8bf961291c2ab1eb221bba403ad48e8e
abhisheksingh98 <writetoabhishek07@gmail.com>
2020-07-05 14:51:14 +0530

    added approach triangle and square

--- GFG/Triangle-and-Square.cpp
+++ GFG/Triangle-and-Square.cpp
@@ -10,6 +10,10 @@
 #include<iostream>
 using namespace std;
 
+//length of base, we can construct length / 2 squares. Since each square is of 2 units, same would be the case of height, there is no need to calculate that again.
+
+// So, for each level of given length we can construct “(length-2)/2” squares. This gives us a base of “(length-2)”
+
 int base(int b){
     int ans = 0;
     while(b>2){
