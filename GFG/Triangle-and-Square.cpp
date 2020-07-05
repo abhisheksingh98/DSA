76042cb3653f8371c88368310723d13aa62dae3b
abhisheksingh98 <writetoabhishek07@gmail.com>
2020-07-05 14:45:45 +0530

    added question triangle and square

--- GFG/Triangle-and-Square.cpp
+++ GFG/Triangle-and-Square.cpp
@@ -1,3 +1,12 @@
+//Question
+// Given a positive integer B denoting the base of a right angled isosceles triangle. Find the maximum number of squares of size 2×2 units that can fit in it.
+
+// Input:
+// First line of input contains an integer T denoting number of test cases. For each test case, there exists one line containing integer B ie-base of right angled isosceles triangle.
+
+// Output:
+// For each test case, print the maximum number of squares that can be fit into the triangle.
+
 #include<iostream>
 using namespace std;
 
