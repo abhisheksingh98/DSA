1fac7c8ce77dc7df41d6c657bc04abf502d49dbe
abhisheksingh98 <writetoabhishek07@gmail.com>
2020-09-17 23:38:20 +0530

    added house robber

---
+++ LeetCode/198. House-robber.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 0) return 0;
+        if(n == 1) return nums[0];
+        int v1 = nums[0], v2 = max(v1, nums[1]);
+        for(int i = 2; i < n; ++i){
+            int tmp = v2;
+            v2 = max(v2, v1 + nums[i]);
+            v1 = tmp;
+        }
+        return v2;
+    }
+};
