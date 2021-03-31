bbcadbf94f410ca539845cc07261317ca2c3a9a2
abhisheksingh98 <writetoabhishek07@gmail.com>
2021-03-31 16:42:21 +0530

    add Container with most water optimized solution

--- LeetCode/11. Container With Most Water.js
+++ LeetCode/11. Container With Most Water.js
@@ -1,4 +1,4 @@
-// Brute Force Approach
+// Brute Force Approach O(n^2)
 
 const maxArea = function(heights){
     let maxArea = 0;
@@ -8,6 +8,25 @@ const maxArea = function(heights){
             const width = j-i;
             const area = height*width;
             maxArea = Math.max(maxArea,area);
+        }
+    }
+    return maxArea;
+}
+
+// Optimized Solution O(n)
+
+const maxArea = function(heights){
+    let i = 0, j = heights.length-1, maxArea = 0;
+    while(i<j){
+        const height = Math.min(heights[i], heights[j]);
+        const width = j-i;
+        const area = height*width;
+        maxArea = Math.max(maxArea,area);
+        if(heights[i] <= heights[j]){
+            i++;
+        }
+        else {
+            j--;
         }
     }
     return maxArea;
